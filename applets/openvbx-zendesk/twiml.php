8d460055a6d214acf7740cb16b2056fd8c3f3aed
Dennis Yu <dennisyu319@gmail.com>
2013-09-20 14:35:49 -0400

    fix variable name for response

--- applets/openvbx-zendesk/twiml.php
+++ applets/openvbx-zendesk/twiml.php
@@ -58,16 +58,16 @@ $phone = normalize_phone_to_E164($_REQUEST['From']);
 
 //zendesk api call
 
-$response = curlWrap("/search.json", array("query" => "type:user phone:" . $phone), "GET");
+$return = curlWrap("/search.json", array("query" => "type:user phone:" . $phone), "GET");
 
-if($response['count'] == 0){
+if($return['count'] == 0){
   //create a new user
-  $response = curlWrap("/users.json", json_encode(array("user" => array("name"=>"Unknown Phone Caller", "phone"=>$phone))), "POST"); 
+  $return = curlWrap("/users.json", json_encode(array("user" => array("name"=>"Unknown Phone Caller", "phone"=>$phone))), "POST"); 
   //create a new ticket in the new user's name
-  $response = curlWrap("/tickets.json", json_encode(array("ticket"=> array("subject" => "New Phone Call From " . $phone, "comment" => array("body" => "call made at " . date('r')), "submitter_id" => $response['id']))), "POST");
-}else if($response['total'] == 1){
+  $return = curlWrap("/tickets.json", json_encode(array("ticket"=> array("subject" => "New Phone Call From " . $phone, "comment" => array("body" => "call made at " . date('r')), "submitter_id" => $return['id']))), "POST");
+}else if($return['total'] == 1){
   //create a new ticket in the name of the current user
-  $response = curlWrap("/tickets.json", json_encode(array("ticket"=> array("subject" => "New Phone Call From " . $phone, "comment" => array("body" => "Call came in at " . date('r')), "submitter_id" => $response[0]['id']))), "POST");
+  $return = curlWrap("/tickets.json", json_encode(array("ticket"=> array("subject" => "New Phone Call From " . $phone, "comment" => array("body" => "Call came in at " . date('r')), "submitter_id" => $return[0]['id']))), "POST");
 }
 
 
